931d057a37ad2df4c8299a37150cb770fd24276d
AranBeitia <arantza.beitia@gmail.com>
2021-11-13 11:59:07 +0100

    comment conflictive code

--- src/views/ProgramaPage.jsx
+++ src/views/ProgramaPage.jsx
@@ -1,29 +1,29 @@
 import HeaderNav from '../components/HeaderNav/HeaderNav'
 import Footer from '../components/Footer'
-import Card from '../components/Card'
+// import Card from '../components/Card'
 
-import data from '../assets/data/programa.json'
+// import data from '../assets/data/programa.json'
 
-import image from '../assets/images/template/meeting-01.jpg'
-import { useState } from 'react'
+// import image from '../assets/images/template/meeting-01.jpg'
+// import { useState } from 'react'
 
 export default function ProgramaPage() {
 
-	let [events, setEvents] = useState(data)
+	// let [events, setEvents] = useState(data)
 
-	let changeEvents = () => {
-		setEvents()
-	}
+	// let changeEvents = () => {
+	// 	setEvents()
+	// }
 
 	return (
 		<div>
 			<HeaderNav />
 			<h1>Programa page</h1>
-			<div className="card-container">
+			{/* <div className="card-container">
 				{events.map((cardInfo, index) => (
 					<Card key={index} title={cardInfo.title} description={cardInfo.description} day={cardInfo.day} hour={cardInfo.hour} image={image} videoLink={cardInfo.videoLink} />
 				))}
-			</div>
+			</div> */}
 			<Footer />
 		</div>
 	)
