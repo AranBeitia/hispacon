cd4b0d37d9d8c361a5012f0c464aa1ae7af19310
CarlosVelilla <carlosvelillaf@hotmail.com>
2021-11-15 12:29:38 +0100

    Change Home Splashpage text

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -6,8 +6,8 @@ import homeVideo from '../assets/video/home.mp4'
 
 export default function PonentesPage() {
 
-	// let text = "Un año más, podemos daros la bienvenida a la HispaCón, el evento de ciencia ficción, fantasía y terror organizado por Pórtico. Este 2021 ha sido complicado y nos ha obligado a volver a organizar la convención de manera telemática. Sin embargo, no queríamos quedarnos sin la posibilidad de vernos las caras, así que también hemos organizado muchas charlas en diferentes sedes de toda la península. Salamanca, Zaragoza, Málaga, Alicante, A Coruña, Barcelona y Madrid son las ciudades en las que podréis acudir a las charlas. Y a los que estas ciudades os quedan lejos, no os preocupéis. ¡Estamos seguros de que podremos encontrarnos cara a cara en 2022! De momento, os invitamos a disfrutar de todas las actividades preparadas para este año."
-	let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut bibendum erat. Suspendisse et feugiat neque, sed viverra augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque a libero fringilla, auctor augue id, congue dolor."
+	let text = "Te damos la bienvenida a la HispaCón, el evento de ciencia ficción, fantasía y terror organizado por Pórtico. Tenemos charlas, tenemos rol, tenemos premios y tenemos microsedes presenciales en A Coruña, Alicante, Barcelona, Madrid, Málaga, Salamanca, y Zaragoza. Desde cerca o desde lejos, la HispaCón te ofrece algo que te apasionará. Pasa y acomódate."
+	// let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut bibendum erat. Suspendisse et feugiat neque, sed viverra augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque a libero fringilla, auctor augue id, congue dolor."
 
 	return (
 		<>
@@ -15,7 +15,7 @@ export default function PonentesPage() {
 			<Splashpage
 				subtitle="¡Bienvenido!"
 				title="Hispacon 2021"
-				description={lorem}
+				description={text}
 				linkText="Ver programa"
 				link="/programa"
 				video={homeVideo}
