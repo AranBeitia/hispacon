b1998a689921650fcb8387019a5be0d9b3f0ee5f
AranBeitia <arantza.beitia@gmail.com>
2021-11-15 19:16:19 +0100

    feria virtual

--- src/components/FeriaCard/FeriaCard.jsx
+++ src/components/FeriaCard/FeriaCard.jsx
@@ -2,14 +2,18 @@ import './FeriaCard.style.scss'
 
 export default function FeriaCard({ id, name, logo, link, benefit }) {
   return (
-    <div className="meeting-item">
+    <a href={link} target="_blank" rel="noreferrer">
+    <div className="feria-card">
       <div className="thumb feria-card__thumb">
-        <img src={logo} alt={`Logo de ${name}`} />
+        <img className="feria-card__img" src={logo} alt={`Logo de ${name}`} />
       </div>
-      <div className="down-content text-content">
-        <h4><a href={link} target="_blank" rel="noreferrer">{name}</a></h4>
+      <div className="feria-card__down-content">
+        <h4 className="feria-card__title">{name}</h4>
+        <div className="text-content">
         <p className="feria-card__text">{benefit}</p>
+        </div>
       </div>
     </div>
+    </a>
   )
 }
